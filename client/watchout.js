dbd2f60d64c7212163253820983261329a9077bc
seankinzler <sfk27@case.edu>
2016-08-06 17:24:46 -0700

    final push.  changed player into an image object.

--- client/watchout.js
+++ client/watchout.js
@@ -1,5 +1,6 @@
 // start slingin' some d3 here.
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 
+  17, 18, 19, 20, 21, 22, 23, 24, 25];
 var timeCount = 0;
 var highScore = 0;
 var collisions = 0;
@@ -8,23 +9,23 @@ var collisionsCheck = false;
 //Enemy
 var enemies = d3.select('svg').selectAll('image').data(array).enter().append('image')
   .attr('xlink:href', 'shuriken.png')
-  .attr('x', function(d) { return Math.random() * 1000; }).attr('y', function(d) { return Math.random() * 750; })
-  .attr('width', 30).attr('height', 30).attr('id', 'enemy');
+  .attr('x', function(d) { return Math.random() * 1440; }).attr('y', function(d) { return Math.random() * 790; })
+  .attr('width', 40).attr('height', 40).attr('id', 'enemy');
   
   
 
 //Player
 var drag = d3.behavior.drag().on('drag', function(d) {
-  d3.select(this).style('cx', d3.event.x); 
-  d3.select(this).style('cy', d3.event.y);
+  d3.select(this).style('x', d3.event.x); 
+  d3.select(this).style('y', d3.event.y); 
 });
 
-var playerArr = [1];
-var player = d3.select('svg').selectAll('.player').data(playerArr).enter().append('circle')
-  .style('r', 8)
-  .style('cy', (750 / 2))
-  .style('cx', (1000 / 2))
-  .style('fill', 'red').call(drag);
+var playerArr = [26];
+var player = d3.select('svg').selectAll('player').data(playerArr).enter().append('image')
+  .attr('xlink:href', 'pokeball.png')
+  .attr('x', 1440 / 2).attr('y', 790 / 2)
+  .attr('width', 70).attr('height', 70).call(drag);
+
 
 
 setInterval(function() {
@@ -41,7 +42,7 @@ setInterval(function() {
 
     var y2 = player[0][0].getBoundingClientRect()['top'] + 
       player[0][0].getBoundingClientRect()['height'] / 2;           //current Y position for player
-    if (distFormula(x1, y1, x2, y2) <= 30) {
+    if (distFormula(x1, y1, x2, y2) <= 55) {
 
       console.log('you lose!');
       timeCount = 0;
@@ -76,15 +77,12 @@ setInterval(function() {
 
 }, 250);
 
-// d3.select('svg').selectAll('shuriken').data(array).on('mouseover', function(event) {
-//   d3.select(this).style('fill', 'red');
-// });
 var distFormula = function(x1, y1, x2, y2) {
   return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 };
 
 setInterval(function() {
   d3.select('svg').selectAll('image').data(array).transition().duration(750)
-  .style('y', function(d) { return Math.random() * 750; })
-  .style('x', function(d) { return Math.random() * 1000; });
+  .style('y', function(d) { return Math.random() * 790; })
+  .style('x', function(d) { return Math.random() * 1440; });
 }, 1000);
